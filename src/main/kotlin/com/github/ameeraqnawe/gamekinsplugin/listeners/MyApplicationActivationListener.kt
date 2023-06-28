a5afa72d9208014760965ec9da4b2a180cbdbb5f
GitHub Action <action@github.com>
2023-06-28 12:38:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/ameeraqnawe/gamekinsplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ameeraqnawe/gamekinsplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ameeraqnawe.gamekinsplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
